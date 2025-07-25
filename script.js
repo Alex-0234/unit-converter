2e9af53f383ba8d1baa6b49848fb358360631460
Alex <alex.liska04@seznam.cz>
2025-07-25 11:48:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 function convert() {
     let output = null
     let inputOption = document.querySelector(".selectOption").value;
-    let inputValue = document.querySelector(".inputValue").value;
-    if (inputOption === "cm to mm"){
-        output = inputValue * 10;
+    let inputValue = parseFloat(document.querySelector(".inputValue").value);
+
+    switch (inputOption) {
+        case "mm to cm":
+            output = inputValue / 10 + "cm";
+        break;
+         case "cm to mm":
+            output = inputValue * 10 + "mm";
+        break;
+         case "pounds to kg":
+            output = inputValue * 0.453592 + "kg";
+        break;
+         case "kg to pounds":
+            output = inputValue * 2.20462 + "pounds";
+        break;
+         case "g to kg":
+            output = inputValue / 1000 + "kg";
+        break;
+         case "kg to g":
+            output = inputValue * 1000 + "g";
+        break;
+
     }
-    else if (inputOption === "pounds to kg") {
-       output = inputValue * 0.453592;
-    }
-    else if (inputOption === "kg to pounds"){
-        output = inputValue * 2.20462;
-    }
-    else if (inputOption === "mm to cm"){
-        output = inputValue / 10;
-    }
-    else if (inputOption === "g to kg"){
-        output = inputValue / 1000;
-    }
-    else if (inputOption === "kg to g"){
-        output = inputValue * 1000;
-    }
-    document.getElementById("result").textContent = "Result: " + output;
-    output = null;
-    
-    inputValue = parseFloat(inputValue);
+     document.getElementById("result").textContent = "Result: " + output;
+    output = null;  
 
 if (isNaN(inputValue)) {
     document.getElementById("result").textContent = "Please enter a valid number.";
     return;
 }
+
 }
